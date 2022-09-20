ddf793217d9163877139898d712b24a92068094b
David <Rigby335@hotmail.com>
2022-09-19 17:10:15 -0700

    titleCase and oddishOrEvenish tests pass

--- day-01/index.js
+++ day-01/index.js
@@ -4,4 +4,26 @@ function reverseSentence(str) {
     .map((word) => word.split('').reverse().join(''))
     .join(' ');
 }
-module.exports = { reverseSentence };
+
+function titleCase(sentence) {
+  return sentence
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.substring(1).toLowerCase())
+    .join(' ');
+}
+
+function oddishOrEvenish(number) {
+  let num = 0;
+  String(number)
+    .split('')
+    .map((number) => (num += Number(number)));
+  return num % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+
+function at(arr, index) {
+  if (arr.length === 0) {
+    return at;
+  }
+}
+
+module.exports = { reverseSentence, titleCase, oddishOrEvenish, at };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,9 +1,22 @@
-const { reverseSentence } = require('./index');
+const { reverseSentence, titleCase, oddishOrEvenish, at } = require('./index');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
     it('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
+    });
+    it('should capitalize first letter', () => {
+      expect(titleCase('alchemy ROCKS goLD')).toBe('Alchemy Rocks Gold');
+    });
+    it('should determines if a number is Oddish or Evenish', () => {
+      expect(oddishOrEvenish('121')).toBe('Evenish');
+    });
+    it('should return the item at corresponding index', () => {
+      const array1 = at(['a', 'b', 'c', 'd', 'e'], 1);
+      const array2 = at(['a', 'b', 'c', 'd', 'e'], -2);
+
+      expect(array1).toBe('b');
+      expect(array2).toBe('d');
     });
   });
 });
