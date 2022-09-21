e8fbd09c2b29ad67a12b4af4aa53c9df1c2f0fed
David <Rigby335@hotmail.com>
2022-09-21 13:27:03 -0700

    testing enqueue

--- day-02/Queue.js
+++ day-02/Queue.js
@@ -1,3 +1,28 @@
-class Queue {}
+const { Stack } = require('./Stack');
+
+class Queue {
+  #list = [];
+
+  constructor(initialList) {
+    if (initialList) this.#list = initialList;
+  }
+
+  enqueue(item) {
+    this.#list.push(item);
+  }
+
+  dequeue() {
+    return this.#list.shift();
+  }
+
+  hasNext() {}
+
+  get list() {
+    return this.#list;
+  }
+}
+
+const newStack = new Stack(['fox', 'goose', 'lizard']);
+newStack.push('llama');
 
 module.exports = { Queue };
