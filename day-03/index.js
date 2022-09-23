30276b6569070903225e428010a05e40cb41eeba
David <Rigby335@hotmail.com>
2022-09-23 09:11:54 -0700

    findPerson returns people nodes in the correct shape and a tree with correct branches

--- day-03/index.js
+++ day-03/index.js
@@ -23,8 +23,47 @@ class BinaryTreeNode {
   }
 }
 
-module.exports = { BinaryTreeNode };
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
 
-//const hashmap = Array.from('abcdefghijklmnopqrstuvwxyz');
-// const lowercase = node.value.toString().toLowerCase();
-// const letterIndex = hashmap.indexOf(lowercase);
+  add(node) {
+    if (node.value === this.value) return null;
+    if (node.value < this.value) {
+      if (this.left == null) {
+        this.left = node;
+      } else {
+        this.left.add(node);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = node;
+      } else {
+        this.right.add(node);
+      }
+    }
+  }
+
+  findPerson(name) {
+    if (name === this.value) return this.person;
+    if (name < this.value) {
+      if (this.left == null) {
+        return null;
+      } else {
+        return this.left.findPerson(name);
+      }
+    } else {
+      if (this.right == null) {
+        return null;
+      } else {
+        return this.right.findPerson(name);
+      }
+    }
+  }
+}
+
+module.exports = { BinaryTreeNode, PersonTreeNode };
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,4 +1,4 @@
-const { BinaryTreeNode } = require('.');
+const { BinaryTreeNode, PersonTreeNode } = require('.');
 
 describe('node based data structures', () => {
   it('should return a binary tree', () => {
@@ -10,13 +10,47 @@ describe('node based data structures', () => {
     B.add(A);
     B.add(D);
     B.add(C);
-    console.log('B', B);
-    console.log('A', A);
-    console.log('D binary tree structure', D);
-    console.log('C', C);
-    console.log('B', B);
 
     expect(B.left.value).toEqual('A');
     expect(B.right.value).toEqual('D');
+  });
+  it('should return a person node', () => {
+    const Jones = new PersonTreeNode({
+      name: 'Jones',
+      phone: '555-1212',
+      address: '123 Main St',
+    });
+    const Brook = new PersonTreeNode({
+      name: 'Brook',
+      phone: '555-1212',
+      address: '123 Main St',
+    });
+    const Smith = new PersonTreeNode({
+      name: 'Smith',
+      phone: '555-1212',
+      address: '123 Main St',
+    });
+    const Nelson = new PersonTreeNode({
+      name: 'Nelson',
+      phone: '555-1212',
+      address: '123 Main St',
+    });
+
+    Jones.add(Brook);
+    Jones.add(Smith);
+    Jones.add(Nelson);
+    console.log('Jones', Jones);
+    Jones.findPerson('Brook');
+    Jones.findPerson('Smith');
+
+    expect(Jones.findPerson('Brook')).toEqual({
+      name: 'Brook',
+      phone: '555-1212',
+      address: '123 Main St',
+    });
+
+    expect(Jones.left.value).toEqual('Brook');
+    expect(Jones.right.value).toEqual('Smith');
+    expect(Jones.right.left.value).toEqual('Nelson');
   });
 });
